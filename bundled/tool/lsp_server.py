25deb161fa2fd2ec944c67d908f69f62449c36e4
Christoph Waffler <waffler.christoph@gmail.com>
2023-05-19 00:46:45 +0200

    remove unused code and comments

--- bundled/tool/lsp_server.py
+++ bundled/tool/lsp_server.py
@@ -113,16 +113,6 @@ def _linting_helper(document: workspace.Document) -> list[lsp.Diagnostic]:
     return _parse_output_using_json(result.stdout) if result.stdout else []
 
 
-# TODO: If your linter outputs in a known format like JSON, then parse
-# accordingly. But incase you need to parse the output using RegEx here
-# is a helper you can work with.
-# flake8 example:
-# If you use following format argument with flake8 you can use the regex below to parse it.
-# TOOL_ARGS += ["--format='%(row)d,%(col)d,%(code).1s,%(code)s:%(text)s'"]
-# DIAGNOSTIC_RE =
-#    r"(?P<line>\d+),(?P<column>-?\d+),(?P<type>\w+),(?P<code>\w+\d+):(?P<message>[^\r\n]*)"
-DIAGNOSTIC_RE = re.compile(r"")
-
 def _parse_output_using_json(content: str) -> Sequence[lsp.Diagnostic]:
     """Parse json linter messages and return LSP diagnostic object for each message"""
     diagnostics = []
@@ -147,41 +137,6 @@ def _parse_output_using_json(content: str) -> Sequence[lsp.Diagnostic]:
     
     return diagnostics    
 
-# def _parse_output_using_regex(content: str) -> list[lsp.Diagnostic]:
-#     lines: list[str] = content.splitlines()
-#     diagnostics: list[lsp.Diagnostic] = []
-
-#     # TODO: Determine if your linter reports line numbers starting at 1 (True) or 0 (False).
-#     line_at_1 = False
-#     # TODO: Determine if your linter reports column numbers starting at 1 (True) or 0 (False).
-#     column_at_1 = True
-
-#     line_offset = 1 if line_at_1 else 0
-#     col_offset = 1 if column_at_1 else 0
-#     for line in lines:
-#         if line.startswith("'") and line.endswith("'"):
-#             line = line[1:-1]
-#         match = DIAGNOSTIC_RE.match(line)
-#         if match:
-#             data = match.groupdict()
-#             position = lsp.Position(
-#                 line=max([int(data["line"]) - line_offset, 0]),
-#                 character=int(data["column"]) - col_offset,
-#             )
-#             diagnostic = lsp.Diagnostic(
-#                 range=lsp.Range(
-#                     start=position,
-#                     end=position,
-#                 ),
-#                 message=data.get("message"),
-#                 severity=_get_severity(data["code"], data["type"]),
-#                 code=data["code"],
-#                 source=TOOL_MODULE,
-#             )
-#             diagnostics.append(diagnostic)
-
-#     return diagnostics
-
 
 # TODO: if you want to handle setting specific severity for your linter
 # in a user configurable way, then look at look at how it is implemented
@@ -197,77 +152,6 @@ def _get_severity(*_codes: list[str]) -> lsp.DiagnosticSeverity:
 # **********************************************************
 # Linting features end here
 # **********************************************************
-
-# TODO: If your tool is a formatter then update this section.
-# Delete "Formatting features" section if your tool is NOT a
-# formatter.
-# **********************************************************
-# Formatting features start here
-# **********************************************************
-#  Sample implementations:
-#  Black: https://github.com/microsoft/vscode-black-formatter/blob/main/bundled/tool
-
-
-# @LSP_SERVER.feature(lsp.TEXT_DOCUMENT_FORMATTING)
-# def formatting(params: lsp.DocumentFormattingParams) -> list[lsp.TextEdit] | None:
-#     """LSP handler for textDocument/formatting request."""
-#     # If your tool is a formatter you can use this handler to provide
-#     # formatting support on save. You have to return an array of lsp.TextEdit
-#     # objects, to provide your formatted results.
-
-#     document = LSP_SERVER.workspace.get_document(params.text_document.uri)
-#     edits = _formatting_helper(document)
-#     if edits:
-#         return edits
-
-#     # NOTE: If you provide [] array, VS Code will clear the file of all contents.
-#     # To indicate no changes to file return None.
-#     return None
-
-
-# def _formatting_helper(document: workspace.Document) -> list[lsp.TextEdit] | None:
-#     # TODO: For formatting on save support the formatter you use must support
-#     # formatting via stdin.
-#     # Read, and update_run_tool_on_document and _run_tool functions as needed
-#     # for your formatter.
-#     result = _run_tool_on_document(document, use_stdin=True)
-#     if result.stdout:
-#         new_source = _match_line_endings(document, result.stdout)
-#         return [
-#             lsp.TextEdit(
-#                 range=lsp.Range(
-#                     start=lsp.Position(line=0, character=0),
-#                     end=lsp.Position(line=len(document.lines), character=0),
-#                 ),
-#                 new_text=new_source,
-#             )
-#         ]
-#     return None
-
-
-# def _get_line_endings(lines: list[str]) -> str:
-#     """Returns line endings used in the text."""
-#     try:
-#         if lines[0][-2:] == "\r\n":
-#             return "\r\n"
-#         return "\n"
-#     except Exception:  # pylint: disable=broad-except
-#         return None
-
-
-# def _match_line_endings(document: workspace.Document, text: str) -> str:
-#     """Ensures that the edited text line endings matches the document line endings."""
-#     expected = _get_line_endings(document.source.splitlines(keepends=True))
-#     actual = _get_line_endings(text.splitlines(keepends=True))
-#     if actual == expected or actual is None or expected is None:
-#         return text
-#     return text.replace(actual, expected)
-
-
-# **********************************************************
-# Formatting features ends here
-# **********************************************************
-
 
 # **********************************************************
 # Required Language Server Initialization and Exit handlers.
